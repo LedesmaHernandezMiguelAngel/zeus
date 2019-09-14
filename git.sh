63beee8058cc0cc88c6d087250666115ed103e81
LedesmaHernandezMiguelAngel <miguelangelledesmahernandez665@gmail.com>
2019-09-14 01:20:39 -0500

    caca de la vaca

--- git.sh
+++ git.sh
@@ -7,8 +7,6 @@ echo "git add";
 git add . 
 echo "git commit";
 git commit -m "caca de la vaca"
-echo "git carpeta";
-git remote add origin https://github.com/LedesmaHernandezMiguelAngel/zeus.git
 echo "git push";
 git push -u origin master
 
